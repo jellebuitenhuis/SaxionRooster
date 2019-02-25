a55bec1dfa419d498062f83657750a23351078fb
jellebuitenhuis <413247@student.saxion.nl>
2019-02-25 16:28:24 +0100

    Eerste werkende versie, rooster in xml file

---
+++ src/Tester.java
@@ -0,0 +1,57 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.*;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+
+public class Tester {
+        public static void main(String[] args) throws Exception {
+            WebClient webClient = new WebClient(BrowserVersion.BEST_SUPPORTED); //Initiate a WebClient variable.
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getCookieManager().setCookiesEnabled(true);
+            webClient = login(webClient);
+            HtmlPage currentPage = webClient.getPage("https://roosters.saxion.nl/schedule/week:0/group:EHI1VSc");
+            webClient.waitForBackgroundJavaScript(10000);
+            WebResponse response = currentPage.getWebResponse();
+            currentPage = webClient.getPage("https://engine.surfconext.nl:443/authentication/sp/consume-assertion");
+            webClient.waitForBackgroundJavaScript(10000);
+            String content = response.getContentAsString();
+            currentPage = webClient.getPage("https://roosters.saxion.nl/schedule/week:0/group:EHI1VSc");
+
+            PrintWriter out = new PrintWriter("rooster.html");
+            out.println(currentPage.asXml());
+            System.out.println(currentPage.asText());
+
+        }
+
+    private static WebClient login(WebClient webClient) throws Exception
+    {
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        webClient.getCookieManager().setCookiesEnabled(true);
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+
+        HtmlPage currentPage = webClient.getPage("https://leren.saxion.nl/webapps/bb-auth-provider-shibboleth-bb_bb60/execute/shibbolethLogin"); //Load page at the STRING address.
+
+        String user;
+        String pass;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your username?");
+        user = sc.nextLine();
+        System.out.println("What is your password?");
+        pass = sc.nextLine();
+
+        HtmlForm htmlForm = currentPage.getFirstByXPath("//*[@id=\"loginForm\"]");
+        htmlForm.getInputByName("UserName").setValueAttribute(user);
+        htmlForm.getInputByName("Password").setValueAttribute(pass);
+        HtmlSpan htmlButton = htmlForm.getFirstByXPath("//*[@id=\"submitButton\"]");
+        currentPage = (HtmlPage) htmlButton.click();
+        webClient.waitForBackgroundJavaScript(10000);
+
+        return webClient;
+    }
+}
